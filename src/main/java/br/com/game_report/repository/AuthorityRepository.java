82a1e928a6b65035c886d97429d9dc6816c4f2af
Tiago Severo <tiago.estrazulas@sc.senai.br>
2023-02-17 19:57:27 -0300

    projeto inicial jhipster.

---
+++ src/main/java/br/com/game_report/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package br.com.game_report.repository;
+
+import br.com.game_report.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/br/com/game_report/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package br.com.game_report.web.rest;
