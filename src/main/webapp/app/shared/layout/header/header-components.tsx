c67fd94605003cdce8c9df0654cba301dd2e2634
Tiago Severo <tiago.estrazulas@sc.senai.br>
2023-02-23 13:37:45 -0300

    Criando componentes para acesso a página de GameReport e Player em header-componentes.

--- src/main/webapp/app/shared/layout/header/header-components.tsx
+++ src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -4,6 +4,8 @@ import { Translate } from 'react-jhipster';
 import { NavItem, NavLink, NavbarBrand } from 'reactstrap';
 import { NavLink as Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { SiPokemon } from 'react-icons/si';
+import { MdCatchingPokemon } from 'react-icons/md';
 
 export const BrandIcon = props => (
   <div {...props} className="brand-icon">
@@ -27,6 +29,28 @@ export const Home = () => (
       <FontAwesomeIcon icon="home" />
       <span>
         <Translate contentKey="global.menu.home">Home</Translate>
+      </span>
+    </NavLink>
+  </NavItem>
+);
+
+export const GameReport = () => (
+  <NavItem>
+    <NavLink tag={Link} to="/game-report" className="d-flex align-items-center">
+      <SiPokemon />
+      <span>
+        <Translate contentKey="global.menu.entities.gameReport">GameReport</Translate>
+      </span>
+    </NavLink>
+  </NavItem>
+);
+
+export const Player = () => (
+  <NavItem>
+    <NavLink tag={Link} to="/player" className="d-flex align-items-center">
+      <MdCatchingPokemon />
+      <span>
+        <Translate contentKey="global.menu.entities.player">Player</Translate>
       </span>
     </NavLink>
   </NavItem>
